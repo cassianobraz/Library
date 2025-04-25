fd6e452e9b0bcc1c37f941e7778e9fd62eedfd75
cassianobraz <cassanopb@gmail.com>
2025-04-25 01:15:46 -0400

    create new project

--- TechLibrary/TechLibrary.Api/Program.cs
+++ TechLibrary/TechLibrary.Api/Program.cs
@@ -1,18 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddRouting(options => options.LowercaseUrls = true);
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
