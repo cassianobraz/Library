e18e56feafe0140eb8ffa98b01171fea3a13a6cf
cassianobraz <cassanopb@gmail.com>
2025-05-05 23:57:06 -0400

    feat password crypt and accesstoken

--- TechLibrary/TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary/TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TechLibrary.Api.Domain.Entities;
 
-namespace TechLibrary.Api.Infraestructure;
+namespace TechLibrary.Api.Infraestructure.DataAccess;
 
 public class TechLibraryDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=C:\\Users\\cassa\\OneDrive\\Documentos\\C#\\ROCKET\\TechLibraryDb.db");
+        optionsBuilder.UseSqlite("Data Source=C:\\Cassiano\\ROCKET\\TechLibraryDb.db");
     }
 }
---
+++ TechLibrary/TechLibrary.Api/Infraestructure/Security/Tokens/Access/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure.Security.Tokens.Access;
+
+public class JwtTokenGenerator
+{
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(60),
+            Subject = new ClaimsIdentity(claims),
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private SymmetricSecurityKey SecurityKey()
+    {
+        var signingKey = "DvJM3eMsG36ZuR3Eg4iEH4rFcxfjt5dl";
+
+        var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+
+        return new SymmetricSecurityKey(symmetricKey);
+    }
+}
